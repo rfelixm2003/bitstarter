d00c5514da5813f27f41bd248ac4de70fcf94d4c
Rodrigo Felix <rfelixm2003@yahoo.com.mx>
2013-07-11 12:16:10 +0100

    include working code from test

--- web.js
+++ web.js
@@ -1,17 +1,31 @@
 var express = require('express');
+var fs = require('fs');
+var content = require('buffer');
 
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
-app.get('/', function(request, data) {
+var content;
+
+app.get('/', function(request, content) {
   
-  fs.readFile('./index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
+fs.readFile('./index.html', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    content = data;
+
+    processFile();
 });
 
+function processFile() {
+    console.log(content);
+    console.log(content.toString('utf8'));
+};  
+
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
