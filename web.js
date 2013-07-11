53f314a99222af45c44d83e221cade73fac82415
Rodrigo Felix <rfelixm2003@yahoo.com.mx>
2013-07-11 11:37:13 +0100

    Added fs instruction

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('/', function(request, data) {
+  
+  fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
 });
 
 var port = process.env.PORT || 5000;
