cff72b58315e5ae741333a26c7f434fb71670c7d
Rodrigo Felix <rfelixm2003@yahoo.com.mx>
2013-07-11 12:46:32 +0100

    a ver que onda

--- web.js
+++ web.js
@@ -8,20 +8,12 @@ var fs = require('fs');
 
 var content;
 
-app.get('/', function(request, content) {
+app.get('/', function(request, response) {
   
-fs.readFile('./index.html', function read(err, content) {
-    if (err) {
-        throw err;
-    }
-    content = data;
-    console.log(content.toString('utf8'));
-   
-});
+ response.send("hello");
 
 
-});
-
+});//app.get end
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
